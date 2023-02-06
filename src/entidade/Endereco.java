a479014a7e87ccf5a41f52228c36a56ab336b8bd
Paulo Costa <pjrc83@gmail.com>
2023-02-06 17:07:17 -0300

    FEAT - Classe Endereço

---
+++ src/entidade/Endereco.java
@@ -0,0 +1,29 @@
+package entidade;
+
+public class Endereco {
+String cep;
+String logradouro;
+String bairro;
+String localidade;
+String uf;
+
+public String getLogradouro() {
+	return logradouro;
+}
+public String getBairro() {
+	return bairro;
+}
+public String getLocalidade() {
+	return localidade;
+}
+public String getUf() {
+	return uf;
+}
+@Override
+public String toString() {
+	return "Endereco logradouro=" + logradouro + ", bairro=" + bairro + ", localidade=" + localidade
+			+ ", uf=" + uf ;
+}
+
+
+}
